3d3b7b1ad9926cd8de9b064474f98d92a0367834
Someshthakur <someshthakur33@gmail.com>
2017-01-08 17:14:44 +0530

    Add "CMA" instruction. Signed-Off by: Somesh Thakur <someshthakur33@gmail.com>

--- src/assemtohex/AssemToHex.java
+++ src/assemtohex/AssemToHex.java
@@ -669,6 +669,13 @@ public class AssemToHex extends javax.swing.JFrame {
 
                     break;
 
+                case "CMA":
+                    hexTxtArea.append("2F\n");
+                    correctFormatInstruction = true;
+                    i--;
+
+                    break;
+
                 case "STA":
                     boolean staaddr = Pattern.matches("\\d*", wordsArray[i + 1].substring(0, wordsArray[i + 1].length() - 1));
                     if (staaddr) {
