83c8ea92c3db49d10f8f3b6c69c0f19482c94275
Someshthakur <someshthakur33@gmail.com>
2017-01-08 17:18:03 +0530

    Add "RAR" instruction. Signed-Off by: Somesh Thakur <someshthakur33@gmail.com>

--- src/assemtohex/AssemToHex.java
+++ src/assemtohex/AssemToHex.java
@@ -690,6 +690,13 @@ public class AssemToHex extends javax.swing.JFrame {
 
                     break;
 
+                case "RAR":
+                    hexTxtArea.append("1F\n");
+                    correctFormatInstruction = true;
+                    i--;
+
+                    break;
+
                 case "STA":
                     boolean staaddr = Pattern.matches("\\d*", wordsArray[i + 1].substring(0, wordsArray[i + 1].length() - 1));
                     if (staaddr) {
