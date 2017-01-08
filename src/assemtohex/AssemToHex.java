59ec7123fa9f1a54ae3a8eb486e0934a43e21d9e
Someshthakur <someshthakur33@gmail.com>
2017-01-08 17:19:48 +0530

    Add "RAL" instruction. Signed-Off by: Somesh Thakur <someshthakur33@gmail.com>

--- src/assemtohex/AssemToHex.java
+++ src/assemtohex/AssemToHex.java
@@ -711,6 +711,13 @@ public class AssemToHex extends javax.swing.JFrame {
 
                     break;
 
+                case "RAL":
+                    hexTxtArea.append("17\n");
+                    correctFormatInstruction = true;
+                    i--;
+
+                    break;
+
                 case "STA":
                     boolean staaddr = Pattern.matches("\\d*", wordsArray[i + 1].substring(0, wordsArray[i + 1].length() - 1));
                     if (staaddr) {
