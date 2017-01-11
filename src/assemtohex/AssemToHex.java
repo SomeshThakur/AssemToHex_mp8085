3a2669394b7ee767d1ec2ba102377e5be797e3a0
Someshthakur <someshthakur33@gmail.com>
2017-01-11 23:30:26 +0530

    Fix "STA" showing invalid address for valid one! Signed-Off by: Somesh Thakur <someshthakur33@gmail.com>

--- src/assemtohex/AssemToHex.java
+++ src/assemtohex/AssemToHex.java
@@ -1066,7 +1066,7 @@ public class AssemToHex extends javax.swing.JFrame {
 
                 case "STA":
                     boolean staaddr = Pattern.matches("\\d*", wordsArray[i + 1].substring(0, wordsArray[i + 1].length() - 1));
-                    if (wordsArray[i + 1].endsWith("H") && wordsArray[i + 1].length() > 5 || wordsArray[i + 1].endsWith("H") && wordsArray[i + 1].length() <= 4 || (!wordsArray[i + 1].endsWith("H")) && wordsArray[i + 1].length() >= 5 || (!wordsArray[i + 1].endsWith("H")) && wordsArray[i + 1].length() <= 4) {
+                    if (wordsArray[i + 1].endsWith("H") && wordsArray[i + 1].length() > 5 || wordsArray[i + 1].endsWith("H") && wordsArray[i + 1].length() <= 4 || (!wordsArray[i + 1].endsWith("H")) && wordsArray[i + 1].length() >= 5 || (!wordsArray[i + 1].endsWith("H")) && wordsArray[i + 1].length() < 4) {
                         hexTxtArea.append("Invalid Address!\n");
                         correctFormatInstruction = true;
                     } else if (staaddr) {
